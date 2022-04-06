769367825a61fb80f13ffd26c9e24af2928c1b1b
calderonraul <raulcalderon@ravn.co>
2022-04-06 11:08:47 -0600

    to the moon

--- src/Main.kt
+++ src/Main.kt
@@ -1,4 +1,38 @@
 
 fun main(args: Array<String>) {
+    val sim = Simulation()
 
+    // Load Items for Phase-1 and Phase-2
+    val phase1 = sim.loadItems("src/phase1.txt")
+    val phase2 = sim.loadItems("src/phase2.txt")
+
+
+    // Load a fleet of U1 rockets for Phase-1 and then for Phase-2
+    val u1FleetPhase1 = sim.loadU1(phase1)
+    val u1FleetPhase2 = sim.loadU1(phase2)
+
+
+    // Run the simulation using the fleet of U1 rockets and display the total budget required
+    println("\nU1 rocket cost = 100")
+    val budgetU1phase1 = sim.runSimulation(u1FleetPhase1)
+    println("Budget of U1 fleet for phase 1 = $budgetU1phase1 (millions)")
+    val budgetU1phase2 = sim.runSimulation(u1FleetPhase2)
+    println("Budget of U1 fleet for phase 2 = $budgetU1phase2 (millions)")
+    println(
+        """Total budget of U1 fleet = ${budgetU1phase1 + budgetU1phase2} (millions)
+"""
+    )
+
+    // Repeat the same for U2 rockets and display the total budget for that
+    val u2FleetPhase1 = sim.loadU2(phase1)
+    val u2FleetPhase2 = sim.loadU2(phase2)
+    println("\nU2 rocket cost = 120")
+    val budgetU2phase1 = sim.runSimulation(u2FleetPhase1)
+    println("Budget of U2 fleet for phase 1 = $budgetU2phase1 (millions)")
+    val budgetU2phase2 = sim.runSimulation(u2FleetPhase2)
+    println("Budget of U2 fleet for phase 2 = $budgetU2phase2 (millions)")
+    println(
+        """Total budget of U1 fleet = ${budgetU2phase1 + budgetU2phase2} (millions)
+"""
+    )
 }
--- src/Simulation.kt
+++ src/Simulation.kt
@@ -1,3 +1,103 @@
-class Simulation (){
+
+import java.io.File
+import java.util.*
+
+class Simulation {
+
+
+    fun loadItems(fileName: String): ArrayList<Item> {
+        val file = File(fileName)
+        val scan = Scanner(file)
+        val items = ArrayList<Item>()
+        while (scan.hasNextLine()) {
+            val line = scan.nextLine()
+            val oneItem = line.split("=".toRegex()).toTypedArray()
+            items.add(Item(oneItem[0], oneItem[1].toInt()))
+        }
+
+        println(fileName + " contains " + items.size + " items")
+
+        return items
+    }
+
+
+    fun loadU1(list: ArrayList<Item>): ArrayList<Rocket> {
+        val fleet = ArrayList<Rocket>()
+        var r: Rocket = U1()
+        var itemCounter = 1
+        var rocketCounter = 1
+        println(
+            """
+    
+    U1 Rocket weight = ${r.Weight}; maxWeight = ${r.MaxWeight}
+    """.trimIndent()
+        )
+        for (i in list) {
+            while (!r.canCarry(i)) {
+                rocketCounter++
+                fleet.add(r)
+                r = U1()
+            }
+            r.carry(i)
+
+
+            itemCounter++
+        }
+        fleet.add(r)
+        println("U1 fleet contains " + fleet.size + " rockets")
+        return fleet
+    }
+
+
+    fun loadU2(list: ArrayList<Item>): ArrayList<Rocket> {
+        val fleet = ArrayList<Rocket>()
+        var r: Rocket = U2()
+        var itemCounter = 1
+        var rocketCounter = 1
+        println("U2 Rocket weight = " + r.Weight.toString() + "; maxWeight = " + r.MaxWeight)
+        for (i in list) {
+            while (!r.canCarry(i)) {
+
+                rocketCounter++
+                fleet.add(r)
+                r = U2()
+            }
+            r.carry(i)
+
+            itemCounter++
+        }
+        fleet.add(r)
+        println(
+            """U2 fleet contains ${fleet.size} rockets
+"""
+        )
+        return fleet
+    }
+
+    fun runSimulation(list: ArrayList<Rocket>): Int {
+        var num = 0 //failed trials of launch/land
+        var indexSuccess = 1
+        for (r in list) {
+            while (!r.launch()) {
+                r.launch()
+                num++
+
+            }
+
+            while (!r.land()) {
+                r.land()
+                num++
+
+            }
+            indexSuccess++
+        }
+        val budget = list[0].cost * (list.size + num)
+        println(
+            list.size.toString() + " rockets and " + num + " extra trials = "
+                    + (list.size + num) + " in total"
+        )
+        return budget
+    }
+
 
 }
